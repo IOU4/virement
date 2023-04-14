d07ec60c29041b53124080294f1f7d8f71481328
IOU4 <ouchaibimad000@gmail.com>
2023-04-14 14:25:24 +0000

    🙃

--- api/src/main/java/eai/virement/virement/VirementResource.java
+++ api/src/main/java/eai/virement/virement/VirementResource.java
@@ -14,9 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMapping(value = "/api/virements", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/virement", produces = MediaType.APPLICATION_JSON_VALUE)
 public class VirementResource {
 
     private final VirementService virementService;
