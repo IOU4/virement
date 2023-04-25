976527a6d647e0f96ab1518477992d2b6b648c67
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2023-04-25 10:46:06 +0100

    Extracted Oauth2 filter to its own Bean for cleaner code.

--- bff/src/main/java/eai/bff/config/ApiClientSecurityConfig.java
+++ bff/src/main/java/eai/bff/config/ApiClientSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClient
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -25,17 +26,20 @@ public class ApiClientSecurityConfig {
 
   @Bean
   public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-    return http
-        .cors(
-            cors -> cors
-                .configurationSource(request -> {
-                  var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
-                  corsConfiguration.addAllowedOrigin("*");
-                  corsConfiguration.addAllowedMethod("*");
-                  corsConfiguration.addAllowedHeader("*");
-                  return corsConfiguration;
-                })
-        ).oauth2Client().and().build();
+    return http.cors(cors -> cors.configurationSource(request -> {
+      var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+      corsConfiguration.addAllowedOrigin("*");
+      corsConfiguration.addAllowedMethod("*");
+      corsConfiguration.addAllowedHeader("*");
+      return corsConfiguration;
+    })).oauth2Client().and().build();
+  }
+
+  @Bean(name = "oauth2Filter")
+  public ServerOAuth2AuthorizedClientExchangeFilterFunction oauth2Filter(ReactiveOAuth2AuthorizedClientManager authorizedClientManager) {
+    var oauthFilter = new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+    oauthFilter.setDefaultClientRegistrationId("keycloak");
+    return oauthFilter;
   }
 
 }
