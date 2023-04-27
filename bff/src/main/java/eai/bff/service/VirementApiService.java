a90d7454e8aa5498071350c6eadb547393811ecc
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2023-04-27 12:03:39 +0100

    Added "/sold" endpoint to handle Server Sent Events from the API to propagate to the UI.

--- bff/src/main/java/eai/bff/service/VirementApiService.java
+++ bff/src/main/java/eai/bff/service/VirementApiService.java
@@ -2,6 +2,8 @@ package eai.bff.service;
 
 import eai.bff.dto.CreateVirementCommand;
 import eai.bff.dto.VirementResponse;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.DeleteExchange;
@@ -11,6 +13,8 @@ import org.springframework.web.service.annotation.PostExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.math.BigDecimal;
+
 @HttpExchange(url = "/virements", accept = "application/json")
 public interface VirementApiService {
 
@@ -19,6 +23,9 @@ public interface VirementApiService {
 
   @GetExchange(url = "/{id}")
   Mono<VirementResponse> getVirementById(@PathVariable String id);
+
+  @GetExchange(url = "/sse/sold", accept = MediaType.TEXT_EVENT_STREAM_VALUE)
+  Flux<ServerSentEvent<BigDecimal>> getSoldSSE();
 
   @PostExchange(contentType = "application/json")
   Mono<Long> createVirement(@RequestBody CreateVirementCommand command);
