11d33733acddb69c06b557bbad037981a3900854
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2023-05-03 09:16:12 +0100

    Added basic skeleton for WebSocket communication.

--- bff/build.gradle.kts
+++ bff/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-integration")
   implementation("org.springdoc:springdoc-openapi-starter-webflux-api")
   implementation("org.springframework.security:spring-security-oauth2-client")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
---
+++ bff/src/main/java/eai/bff/config/ReactiveWebSocketConfig.java
@@ -0,0 +1,39 @@
+package eai.bff.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyRequestUpgradeStrategy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ReactiveWebSocketConfig {
+
+  @Bean
+  public HandlerMapping webSocketHandlerMapping(WebSocketHandler handler) {
+    Map<String, WebSocketHandler> map = new HashMap<>();
+    map.put("/ws/sold", handler);
+
+    SimpleUrlHandlerMapping handlerMapping = new SimpleUrlHandlerMapping();
+    handlerMapping.setUrlMap(map);
+    handlerMapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return handlerMapping;
+  }
+
+  @Bean
+  public ReactorNettyRequestUpgradeStrategy upgradeStrategy() {
+    return new ReactorNettyRequestUpgradeStrategy();
+  }
+
+  @Bean
+  public WebSocketHandlerAdapter webSocketHandlerAdapter() {
+    return new WebSocketHandlerAdapter();
+  }
+
+}
