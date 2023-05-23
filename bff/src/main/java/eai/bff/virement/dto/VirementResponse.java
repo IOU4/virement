0640eb25db1575557a2ec786573be5360e66b22e
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2023-05-23 10:42:21 +0100

    fixed serialization error

--- bff/src/main/java/eai/bff/virement/dto/VirementResponse.java
+++ bff/src/main/java/eai/bff/virement/dto/VirementResponse.java
@@ -3,11 +3,12 @@ package eai.bff.virement.dto;
 import eai.bff.common.dto.BaseId;
 import eai.bff.common.dto.OperationStatus;
 
+import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public record VirementResponse(BaseId<UUID> id, BaseId<UUID> customerAccountSource, BaseId<UUID> customerAccountTarget,
-                               OperationStatus virementOperationStatus, String amount, BaseId<UUID> trackingId,
+                               OperationStatus virementOperationStatus, BigDecimal amount, BaseId<UUID> trackingId,
                                List<String> failureMessages, ZonedDateTime createdAt) {
 }
